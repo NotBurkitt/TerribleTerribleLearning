2655db5b68706d9657ecb481eaf07348ac6b526d
NotBurkitt <williamburkill1@gmail.com>
2020-07-08 17:54:01 +0100

    audio

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+let audio = new Audio("Recording.m4a");
 console.log("message");
-let body = document.querySelector("body")
+let body = document.querySelector("body");
 let button = document.querySelector("#JamesTheButton");
 button.addEventListener("click", () => {
     alert("WARNING")
     setInterval( () => {
         body.style.backgroundColor = `rgb(${random()},${random()},${random()})`
+        audio.play();
         }, 10)
     console.log(button)
 });
 let random = () => {
     return Math.floor(Math.random()*255)
-}
+};
 
