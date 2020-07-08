af68945568e8dfeb691fc7becc90c43df4722338
NotBurkitt <williamburkill1@gmail.com>
2020-07-08 17:45:01 +0100

    WARNING

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 console.log("message");
+let body = document.querySelector("body")
 let button = document.querySelector("#JamesTheButton");
 button.addEventListener("click", () => {
     alert("WARNING")
+    setInterval( () => {
+        body.style.backgroundColor = `rgb(${random()},${random()},${random()})`
+        }, 10)
     console.log(button)
 });
-
+let random = () => {
+    return Math.floor(Math.random()*255)
+}
 
